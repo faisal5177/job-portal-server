0a5024bd147f86ae339929368d4ca3c1d8ced6a5
faisal <mdfaisal51777@gmail.com>
2024-12-25 12:37:07 +0600

    added an

--- index.js
+++ index.js
@@ -59,6 +59,13 @@ async function run() {
       res.send(result);
     });
 
+    app.get('/job-application/jobs/:job_id', async (req, res) => {
+      const jobId = req.params.job_id;
+      const query = { job_id: jobId }
+      const result = await jobApplicationCollection.find(query).toArray();
+      res.send(result);
+  })
+
     app.get('/job-application', async (req, res) => {
       const email = req.query.email;
       const query = { applicant_email: email };
