4d858c2854052d0c2e2821f930e45387c67e5a72
faisal <mdfaisal51777@gmail.com>
2024-12-30 01:37:09 +0600

    added an verifyToken

--- index.js
+++ index.js
@@ -2,6 +2,8 @@ require('dotenv').config();
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const express = require('express');
 const cors = require('cors');
+const jwt = require('jsonwebtoken');
+const cookieParser = require('cookie-parser');
 const app = express();
 const port = process.env.PORT || 5001;
 
@@ -11,8 +13,33 @@ const corsOptions = {
   credentials: true,
 };
 
-app.use(cors(corsOptions));
+app.use(cors({
+  origin: ['http://localhost:5173'],
+  credentials: true,
+}));
 app.use(express.json());
+app.use(cookieParser());
+
+
+const logger = (req, res, next) => {
+  console.log(`inside the logger`);
+  next();
+}
+
+const verifyToken = (req, res, next) => {
+  const token = req?.cookies?.token;
+
+  if(!token) {
+    return res.status(401).send({message:'unAuthorized access'})
+  }
+  jwt.verify(token,process.env.ACCESS_TOKEN_SECRET, (err, decoded) => {
+    if(err){
+      return res.status(401).send({message: 'unauthorized access'})
+    }
+    req.user = decoded;
+    next();
+  })
+}
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.8kzkr.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
 
@@ -26,13 +53,23 @@ const client = new MongoClient(uri, {
 
 async function run() {
   try {
-    await client.connect();
-    await client.db("admin").command({ ping: 1 });
-    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+   
     
     // jobs related apis
     const jobsCollection = client.db('jobPortal').collection('jobs');
     const jobApplicationCollection = client.db('jobPortal').collection('job_applications');
+
+    // Auth related APIs
+     app.post('/jwt', async(req, res) => {
+      const user = req.body;
+      const token = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, {expiresIn:'5h'});
+      res
+         .cookie('token', token,{
+         httpOnly: true,
+         secure: false,
+      })
+      .send({success: true});
+     })
     
     // jobs related APIs
     app.get('/jobs', async (req, res) => {
@@ -66,9 +103,14 @@ async function run() {
       res.send(result);
   })
 
-    app.get('/job-application', async (req, res) => {
+    app.get('/job-application', verifyToken, async (req, res) => {
       const email = req.query.email;
       const query = { applicant_email: email };
+
+      if(req.user.email !== req.query.email){
+        return res.status(403).send({message: 'forbidden access'});
+      }
+
       const result = await jobApplicationCollection.find(query).toArray();
     
       for (const application of result) {
@@ -97,6 +139,9 @@ async function run() {
       const result = await jobApplicationCollection.insertOne(application);
       res.send(result);
     });
+
+    await client.db("admin").command({ ping: 1 });
+    console.log("Pinged your deployment. You successfully connected to MongoDB!");
 
   } catch (error) {
     console.error("Error:", error);
